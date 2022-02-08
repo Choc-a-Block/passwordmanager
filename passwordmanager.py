ad7af1a7d1012e8a534b23531988e8fc4ff6931c
Will Kane <zkapollyon@gmail.com>
2022-02-08 23:26:46 +0000

    Adding comments.

--- passwordmanager.py
+++ passwordmanager.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python3
+#imports
 import hashlib
 import base64
 import os
 from cryptography.fernet import Fernet
 
-hash = hashlib.shake_256()
+#functions
 
+#Used to write to the default file generated when the program is first started.
 def writeDefaultFile():
     o = open("passwords", "w")
     o.write("This is a file for passwordmanager.\n")
     o.close()
 
+#Encodes the passwords file into UTF-8
 def encodeFile():
     o = open("passwords", "r")
     file = o.read()
     encoded = file.encode("utf-8")
     o.close()
  
+#Presents options menu for the user.
 def userChoice():
     choice = int(input("1. Query for password.\n2. Add password to database.\nWhat do you wish to do?"))
     return choice 
 
+#Encrypts the data fed into it using the Fernet key.
 def encryptData(message):
     f = Fernet(key)
     encrypt = f.encrypt(message)
     return encrypt
 
+#Gets and decrypts the data inside the passwords file, then returns the output.
 def getContents():
     o = open("passwords", "r")
     encryptedContents = o.open()
@@ -34,6 +40,8 @@ def getContents():
     contents = f.decrypt(encryptedContentsBytes)
     return contents
 
+#Sets a global variable called key, generates a hash from the users password, 
+#and cuts it down to 32 characters (length of the key required by Fernet), then encodes the hash into a URL-safe, base64 format. (The format required by Fernet)
 def setKey():
     global key
     password = input("Password?")
@@ -42,6 +50,8 @@ def setKey():
     key = base64.urlsafe_b64encode(keyHash)
     print(key)
 
+#Used when the user is trying to retreive an entry from their passwords file.
+#Gets a query, decrypts the main passwords file, then searches it and prints out all matching results.
 def getEntry():
     query = input("Query here:")
     with open("passwords", "r+") as o:
@@ -52,6 +62,8 @@ def getEntry():
             if query in line:
                 print(line)
 
+#Used when the user wishes to add an entry to their passwords file.
+#Gets all relevant information, concatenates it, opens the passwords file and writes the new line, encrypted with the key defined in keySet.
 def addEntry():
     ident = input("What is the website/identifier?")
     email = input("What is the email/username?")
@@ -62,6 +74,8 @@ def addEntry():
         encrypted = f.encrypt(concatStr)
         o.write(encrypted)
 
+#Initial function to be run to detect if the required file "passwords" is present.
+#If the required file is not present, it is created, initial message written and encoded, then encrypted.
 def fileCheck():
     if not os.path.isfile('passwords'):
         os.mknod('passwords')
@@ -77,6 +91,7 @@ def fileCheck():
     else:
         pass
 
+#Main function, simple interpreter of the choice made in userChoice. Exits if the choice is invalid...
 def main(userChoice):
     if userChoice == 1:
         getEntry()
@@ -85,6 +100,7 @@ def main(userChoice):
     else:
         print("Invalid input. Exiting...")
 
+#Main set of function calls.
 fileCheck()
 setKey()
 choice = userChoice()
